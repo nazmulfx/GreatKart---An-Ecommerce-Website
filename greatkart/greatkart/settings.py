0689a9583c102de67b2bb2dd6a87a24ce615fb79
nazmulfx <nazmulhossain664@gmail.com>
2024-04-07 01:14:43 +0600

    MEDIA_URL & MEDIA_ROOT configured

--- greatkart/greatkart/settings.py
+++ greatkart/greatkart/settings.py
@@ -121,12 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+## Media Files Configuratons
+MEDIA_URL = '/images/'
+MEDIA_ROOT = BASE_DIR / 'images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
