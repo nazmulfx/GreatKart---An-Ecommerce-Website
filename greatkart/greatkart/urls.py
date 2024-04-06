04482d10517b9160f24cc0327a9372f24c1ba311
nazmulfx <nazmulhossain664@gmail.com>
2024-04-06 16:41:14 +0600

    static files hundled

--- greatkart/greatkart/urls.py
+++ greatkart/greatkart/urls.py
@@ -16,12 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-from django.http import HttpResponse
-def home(request):
-    return HttpResponse('Home Page')
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name="home")
+    path('', views.home, name="home"),
 ]
