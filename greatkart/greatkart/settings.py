e91c3e9a31649b5623d2476af9f0bbfed821e9f4
nazmulfx <nazmulhossain664@gmail.com>
2024-04-06 23:58:48 +0600

    account app model created

--- greatkart/greatkart/settings.py
+++ greatkart/greatkart/settings.py
@@ -132,3 +132,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+## Custom user model
+AUTH_USER_MODEL = 'accounts.Account'
