f3b97a3df0280f4d70861b3c959918d0fda0a92e
nazmulfx <nazmulhossain664@gmail.com>
2024-04-07 14:56:25 +0600

    Category Model: slug auto complete from category name

--- greatkart/category/models.py
+++ greatkart/category/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=200, unique=True)
-    slug = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField(max_length=1000, blank=True)
     image = models.ImageField(upload_to='photos/category', blank=True)
     
